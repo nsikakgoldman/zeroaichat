d09b06cbc409d249275e7fa139ac005911bd8538
nsikakgoldman <georgegoldman.john.o.245299@unn.edu.ng>
2023-12-11 17:17:46 +0100

    removed config files

--- zeroair/chatintelligence.py
+++ zeroair/chatintelligence.py
@@ -5,15 +5,12 @@ client = OpenAI(
 )
 
 def aiResponse():
-  resp = """
-  Lagos is a bustling city located in southwest Nigeria, positioned along the country's Atlantic coast. It is the largest city in Nigeria and one of the fastest-growing cities in Africa. Named after the Portuguese word for "lakes," Lagos is historically known for its lagoons and waterways. Lagos has a vibrant and diverse culture, influenced by various ethnic groups such as the Yoruba, Igbo, and Hausa. This rich cultural tapestry is evident in the city's art, music, food, and traditional festivals, which attract both locals and tourists. The city is a major economic hub and a significant financial center in Africa. It is home to the Nigerian Stock Exchange, many national and multinational corporations, and an active entrepreneurial scene. This economic growth has also led to an emerging middle class, which has contributed to the city's rapid urbanization and modernization. Lagos is famous for its lively markets, where you can find a wide range of goods, from unique crafts and fabrics to fresh produce and food items. The city is renowned for its street food, with dishes like jollof rice, suya (grilled meat skewers), and puff puff (a type of fried dough) being local favorites. In terms of landmarks, Lagos offers a mix of modern architecture and historical sites. Prominent attractions include the National Museum of Nigeria, Lekki Conservation Centre, Nike Art Gallery, Tafawa Balewa Square, and the iconic Third Mainland Bridge, one of the longest bridges in Africa. However, Lagos faces some challenges as well, such as traffic congestion, rapid urbanization, and inadequate infrastructure in some areas. Efforts are being made to address these issues and improve the quality of life for residents. Overall, Lagos is a vibrant, dynamic, and culturally diverse city with a lot to offer. Its bustling markets, rich cultural heritage, and economic opportunities make it a fascinating place to visit or live in.
-  """
   completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
       # prompt= "tell me about... lagos"
       messages=[
-      {"role": "system", "content": f"You are a helpful assistant.and you answered {resp}"},
-      {"role": "user", "content": "why  It is home to the Nigerian Stock Exchange"}
+      {"role": "system", "content": f"You are only to answer food and grocery related question! "},
+      {"role": "user", "content": "what is the best carbohydrate food in africa? if this question  don't pertains to food and grocery please don't answer"}
     ]
   )
   # print(completion['choices'][0]['message']['content'])
